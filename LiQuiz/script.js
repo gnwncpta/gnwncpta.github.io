df7f8d7550fe26c9de3a9bb4e3c4b77866ecccfc
gnwncpta <gnwncpta@gmail.com>
2021-02-13 23:38:49 +0700

    Update

--- LiQuiz/script.js
+++ LiQuiz/script.js
@@ -1,17 +1,32 @@
-const nav = document.querySelectorAll('header ul li a');
+// WELCOME ALERT
+const darkBG = document.getElementById('dark-bg');
+const welcome = document.querySelector('.welcome');
+const buttonWelcome = welcome.querySelector('.close');
 
-nav.forEach((e) => {
-    e.addEventListener('mouseover', (el) => {
-        const target = el.target;
-        target.classList.add('dot');
-       
-        if(target.classList.contains('sign-up')){
-            target.classList.remove('dot');
-        }
+document.body.classList.add('stop-scrolling');
 
-        target.addEventListener('mouseleave', (e) => {
-            target.classList.remove('dot');
-        });
+if(localStorage.getItem('welcome') == 'YES'){
+    document.body.removeChild(welcome);
+    document.body.removeChild(darkBG);
+    document.body.classList.remove('stop-scrolling');
+} else {
+    buttonWelcome.addEventListener('click', (e) => {
+        // welcome.style.display = 'none';
+        // darkBG.style.display = 'none';
+    
+        document.body.removeChild(welcome);
+        document.body.removeChild(darkBG);
+        document.body.classList.remove('stop-scrolling');
+        
+        localStorage.setItem('welcome', 'YES');
+        e.preventDefault();
+
+        // BEFORE WORK
+        // if(localStorage.getItem('welcome', 'YES')){
+        //     welcome.style.disply = 'none';
+        //     darkBG.style.disply = 'none';
+        // } else {
+        //     const welcomeHide = localStorage.setItem('welcome', 'YES')
+        // }
     });
-});
-
+}
