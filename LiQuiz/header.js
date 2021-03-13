1c861b72b24dbe4b882301ccfcc181c49dff530e
gnwncpta <gnwncpta@gmail.com>
2021-03-13 14:23:41 +0700

    Update UI navbar

--- LiQuiz/header.js
+++ LiQuiz/header.js
@@ -1,11 +1,16 @@
 const nav = document.querySelectorAll('header ul li a');
 
 nav.forEach((e) => {
+    console.log(e);
     e.addEventListener('mouseover', (el) => {
         const target = el.target;
         target.classList.add('dot');
        
         if(target.classList.contains('sign-up')){
+            target.classList.remove('dot');
+        }
+
+        if(target.classList.contains('login')){
             target.classList.remove('dot');
         }
 
